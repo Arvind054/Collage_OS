e55da6ec50f4b0d72337982de97f893c40a6471c
Anurag Pramod Shingare <anuragshingare2005@gmail.com>
2025-04-18 21:36:14 +0530

    merge: Added rules and regulation bot

--- utils/actions.tsx
+++ utils/actions.tsx
@@ -1,12 +1,12 @@
 "use server"
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { ChatGroq } from "@langchain/groq";
-import { InMemoryChatMessageHistory } from "@langchain/core/chat_history";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { RunnableWithMessageHistory } from "@langchain/core/runnables";
-import { HumanMessage } from "@langchain/core/messages";
-import { AIMessage } from "@langchain/core/messages";
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from "@google/genai"; // arvind
+// import { ChatGroq } from "@langchain/groq";
+// import { InMemoryChatMessageHistory } from "@langchain/core/chat_history";
+// import { ChatPromptTemplate } from "@langchain/core/prompts";
+// import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+// import { HumanMessage } from "@langchain/core/messages";
+// import { AIMessage } from "@langchain/core/messages";
 
 
 /**
@@ -52,83 +52,7 @@ async function chatResponse(prompt: string) {
 };
 
 
-
-
-
-
-async function langchainChatResponse(text: string) {
-  try {
-    // LANGUAGE MODEL TO BE USED.
-    const model = new ChatGroq({
-      model: "mixtral-8x7b-32768",
-      temperature: 0
-    });
-    // THIS IS AN JS OBJECT WHICH WILL STORE OUR MESSAGE HISTORY.
-    const messageHistories: Record<string, InMemoryChatMessageHistory> = {};
-
-    // THIS IS OUR TEMPLATE TO GENERATE THE RESPONSE.
-    const prompt = ChatPromptTemplate.fromMessages([
-      [
-        "system",
-        `You are a helpful assistant who remembers all details the user shares with you.`,
-      ],
-      ["placeholder", "{chat_history}"],
-      ["human", "{input}"],
-    ]);
-
-    // THIS IS USED TO CONNECT OUR LLM MODEL WITH OUTPUT PARSER
-    const chain = prompt.pipe(model);
-
-    // HERE WE CAN PROVIDE SPECIFIC DETAILS RELATED TO US OR FOR THIS APP.
-    const messages = [
-      new HumanMessage({ content: "hi! I'm anurag" }),
-      new AIMessage({ content: "hi!" }),
-    ];
-
-    // THIS FUNCTION WILL ENCODE THE RESPONSE FROM AI AS WELL THE INPUT FROM USER. THIS FUNC REQUIRED THE SESSION ID.
-    const withMessageHistory = new RunnableWithMessageHistory({
-      runnable: chain,
-      getMessageHistory: async (sessionId) => {
-        if (messageHistories[sessionId] === undefined) {
-          const messageHistory = new InMemoryChatMessageHistory();
-          await messageHistory.addMessages(messages);
-          messageHistories[sessionId] = messageHistory;
-        }
-        return messageHistories[sessionId];
-      },
-      inputMessagesKey: "input",
-      historyMessagesKey: "chat_history",
-    });
-
-
-    // IF WE HAVE AN APP WHICH OPERATES MULTIPLE USER IN SAME APP. WE CAN SAVE THEIR HISTORY WITH THE UNIQUE SESSION ID.
-    const config = {
-      configurable: {
-        sessionId: "abc2",
-      },
-    };
-
-
-    // HERE, WE WILL GET THE RESPONSE FROM AI.
-    const response = await withMessageHistory.invoke(
-      {
-        input: text,
-      },
-      config
-    );
-    console.log(response.content);
-    return (response.content);
-  }
-  catch (error) {
-    console.log(error);
-
-  };
-
-
-};
-
-
-async function GetResumeATS_Score(ResumeText){
+async function GetResumeATS_Score(ResumeText:any){
   try{
     const ai = new GoogleGenAI({ apiKey: "Your Key Here" });
     const propmt = `This is the text of my prase Reusme \n ${ResumeText}. \n Only provide the Numerical score out of 100 and Nothing Else.`
@@ -145,4 +69,8 @@ async function GetResumeATS_Score(ResumeText){
 
 
 
-export { chatResponse, langchainChatResponse,GetResumeATS_Score };
+
+
+
+
+export { chatResponse,GetResumeATS_Score};
